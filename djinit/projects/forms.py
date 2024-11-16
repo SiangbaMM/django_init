1396f60d0f705250fb6ab9fe9bcc1095d29342a5
SiangbaMM <msiangba@gmail.com>
2024-11-16 22:58:10 +0100

    Update main form

--- djinit/projects/forms.py
+++ djinit/projects/forms.py
@@ -1,7 +1,26 @@
+from typing import Any, Mapping
+from django.core.files.base import File
+from django.db.models.base import Model
 from django.forms import ModelForm 
+from django import forms
+from django.forms.utils import ErrorList
 from .models import Project 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'feature_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags' : forms.CheckboxSelectMultiple(),
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        
+        #self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add title'})
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+        
+        
+        
