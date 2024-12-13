143d8d9b621b918c43ab3ca91ddd77c48ad4430c
SiangbaMM <msiangba@gmail.com>
2024-12-13 14:56:03 +0100

    Complete register and login page

--- djinit/users/views.py
+++ djinit/users/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def loginUser(request):
-    
+    page = 'login'
+    context = {'page':page}
     if request.user.is_authenticated:
         return redirect('profiles')
     
@@ -30,13 +32,36 @@ def loginUser(request):
         else:
             messages.error(request,'Username or password is incorrect')
             
-    return render(request, 'users/login_register.html')
+    return render(request, 'users/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     messages.success(request,'User was logged out!')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        
+        if form.is_valid() :
+            user = form.save(commit=False)  # For tranformation purpose
+            user.username = user.username.lower()
+            user.save()
+            
+            messages.success(request, 'User account was created!')
+            
+            login(request, user) 
+            return redirect('profiles')
+        
+        else:
+            messages.error(request, 'An error has occurred during resgistration!')
+    
+    context = {'page': page, 'form':form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
